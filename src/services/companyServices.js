ed7af6dbeda91077d64dc481b86a9106989df93a
Harsh <harsh821agarwal@gmail.com>
2023-02-06 13:06:53 +0530

    refactor: create utility functions

--- src/services/companyServices.js
+++ src/services/companyServices.js
@@ -1,46 +1,27 @@
 /* eslint-disable camelcase */
-const axios = require('axios')
-const csvToJson = require('csvtojson')
+const utils = require('../utils/companyUtils')
 
 const { Company } = require('../../database/models')
 
 const saveData = async (urlLink) => {
-  const response = await axios.get(urlLink, { responseType: 'blob' })
-  const file = response.data
-  const data = await csvToJson().fromString(file.toString())
+  const data = await utils.getData(urlLink)
   const allCompanies = []
+
   for (const company of data) {
     const { company_id, company_sector } = company
-    const getCompanyDetails = await axios.get(`http://54.167.46.10/company/${company_id}`)
+    const getCompanyDetails = await utils.getCompanyDetails(company_id)
     const companyDetails = getCompanyDetails.data
-    const getSectorDetails = await axios.get(`http://54.167.46.10/sector?name=${company_sector}
-    `)
-
+    const getSectorDetails = await utils.getSectorDetails(company_sector)
     const sectorDetails = getSectorDetails.data
     const data = sectorDetails.find(sector => sector.companyId === company_id)
-    const scoringData = data.performanceIndex.reduce((acc, scores) => {
-      if (scores.key === 'cpi') {
-        acc += scores.value * 10
-      }
-      if (scores.key === 'cf') {
-        acc += scores.value / 10000
-      }
-      if (scores.key === 'mau') {
-        acc += scores.value * 10
-      }
-      if (scores.key === 'roic') {
-        acc += scores.value
-      }
-      return acc
-    }, 0)
-    const companyScore = Number((scoringData / 4).toFixed(2))
+    const score = utils.getScore(data)
     const { name, ceo } = companyDetails
     const companyData = {
       company_id,
       name,
       ceo,
       company_sector,
-      score: companyScore
+      score
     }
 
     await Company.create(companyData)
---
+++ src/utils/companyUtils.js
@@ -0,0 +1,33 @@
+/* eslint-disable camelcase */
+const axios = require('axios')
+const csvToJson = require('csvtojson')
+
+const getData = async (urlLink) => {
+  const response = await axios.get(urlLink, { responseType: 'blob' })
+  const file = response.data
+  return csvToJson().fromString(file.toString())
+}
+const getCompanyDetails = async (company_id) => await axios.get(`http://54.167.46.10/company/${company_id}`)
+
+const getSectorDetails = async (company_sector) => await axios.get(`http://54.167.46.10/sector?name=${company_sector}
+`)
+const getScore = (data) => {
+  const score = data.performanceIndex.reduce((acc, scores) => {
+    if (scores.key === 'cpi') {
+      acc += scores.value * 10
+    }
+    if (scores.key === 'cf') {
+      acc += scores.value / 10000
+    }
+    if (scores.key === 'mau') {
+      acc += scores.value * 10
+    }
+    if (scores.key === 'roic') {
+      acc += scores.value
+    }
+    return acc
+  }, 0)
+  return Number((score / 4).toFixed(2))
+}
+
+module.exports = { getData, getCompanyDetails, getSectorDetails, getScore }
